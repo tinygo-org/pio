0076fea32f22030c106a3da24f868892dc5ee661
soypat <graded.sp@gmail.com>
2023-10-28 00:58:09 -0300

    add blinky (not working)

---
+++ rp2040-pio/examples/blinky/main.go
@@ -0,0 +1,35 @@
+//go:generate pioasm -o go blink.pio blink_pio.go
+
+package main
+
+import (
+	"machine"
+	"time"
+
+	pio "github.com/tinygo-org/pio/rp2040-pio"
+)
+
+func main() {
+	// Sleep to catch prints.
+	time.Sleep(2 * time.Second)
+	Pio := pio.PIO0
+
+	offset, err := Pio.AddProgram(blinkInstructions, blinkOrigin)
+	if err != nil {
+		panic(err.Error())
+	}
+	println("Loaded program at", offset)
+
+	blinkPinForever(Pio.StateMachine(0), offset, machine.LED, 3)
+	blinkPinForever(Pio.StateMachine(1), offset, machine.GPIO6, 4)
+	blinkPinForever(Pio.StateMachine(2), offset, machine.GPIO11, 1)
+	select {}
+}
+
+func blinkPinForever(sm pio.StateMachine, offset uint8, pin machine.Pin, freq uint) {
+	blinkProgramInit(sm, offset, pin)
+	sm.SetEnabled(true)
+	const clockFreq = 125000000
+	println("Blinking", int(pin), "at", freq, "Hz")
+	sm.TxPut(uint32(clockFreq / (2 * freq)))
+}
--- rp2040-pio/examples/tufty/parallel_pio.go
+++ rp2040-pio/examples/tufty/parallel_pio.go
@@ -12,8 +12,8 @@ func parallelST7789Init(sm pio.StateMachine, offset uint8, d0_pin machine.Pin, w
     d0_pin.Configure(machine.PinConfig{Mode: machine.PinPIO0})
     sm.SetConsecutivePinDirs(d0_pin, 8, true)
     cfg := st7789_parallelProgramDefaultConfig(offset)
-    cfg.SetOutPins(d0_pin, 8)
-    cfg.SetSidesetBase(wr_pin)
+    cfg.SetSetPins(d0_pin, 8)
+    cfg.SetSidePins(wr_pin)
     cfg.SetFIFOJoin(pio.FIFO_JOIN_TX)
 	cfg.SetOutShift(false, true, 8)
     maxPIOClk := uint32(32 * machine.MHz)
@@ -38,7 +38,7 @@ const st7789_parallelOrigin = -1
 func st7789_parallelProgramDefaultConfig(offset uint8) pio.StateMachineConfig {
 	cfg := pio.DefaultStateMachineConfig()
 	cfg.SetWrap(offset+st7789_parallelWrapTarget, offset+st7789_parallelWrap)
-	cfg.SetSideset(1, false, false)
+	cfg.SetSideSet(1, false, false)
 	return cfg;
 }
 
