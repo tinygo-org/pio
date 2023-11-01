5475d00aa7d121ca7dd7aec19a0757d95da564cd
soypat <graded.sp@gmail.com>
2023-10-31 23:58:45 -0300

    begin adding SDIO

--- rp2-pio/piolib/all_generate.go
+++ rp2-pio/piolib/all_generate.go
@@ -12,6 +12,8 @@ var errTimeout = errors.New("piolib:timeout")
 //go:generate pioasm -o go spi.pio        spi_pio.go
 //go:generate pioasm -o go ws2812.pio     ws2812_pio.go
 //go:generate pioasm -o go i2s.pio        i2s_pio.go
+//go:generate pioasm -o go sdio.pio       sdio_pio.go
+
 func gosched() {
 	runtime.Gosched()
 }
