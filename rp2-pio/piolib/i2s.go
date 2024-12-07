b256715471d2d04904a2596434532205902d2a8d
deadprogram <ron@hybridgroup.com>
2024-12-07 21:04:14 +0100

    i2s: modify interface to match i2s interface in TinyGo machine package
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- rp2-pio/piolib/i2s.go
+++ rp2-pio/piolib/i2s.go
@@ -1,19 +1,23 @@
-//go:build rp2040 && go1.18
+//go:build rp2040
 
 package piolib
 
 import (
+	"errors"
 	"machine"
 
 	pio "github.com/tinygo-org/pio/rp2-pio"
 )
 
+// I2S is a wrapper around a PIO state machine that implements I2S.
+// Currently only supports writing to the I2S peripheral.
 type I2S struct {
 	sm      pio.StateMachine
 	offset  uint8
 	writing bool
 }
 
+// NewI2S creates a new I2S peripheral using the given PIO state machine.
 func NewI2S(sm pio.StateMachine, data, clockAndNext machine.Pin) (*I2S, error) {
 	sm.TryClaim() // SM should be claimed beforehand, we just guarantee it's claimed.
 	Pio := sm.PIO()
@@ -49,10 +53,12 @@ func NewI2S(sm pio.StateMachine, data, clockAndNext machine.Pin) (*I2S, error) {
 	}
 	// This enables the state machine. Good practice to not require users to do this
 	// since they may be confused why nothing is happening.
-	i2s.Paused(false)
+	i2s.Enable(true)
+
 	return i2s, nil
 }
 
+// SetSampleFrequency sets the sample frequency of the I2S peripheral.
 func (i2s *I2S) SetSampleFrequency(freq uint32) error {
 	freq *= 32 // 32 bits per sample
 	whole, frac, err := pio.ClkDivFromFrequency(freq, machine.CPUFrequency())
@@ -63,12 +69,24 @@ func (i2s *I2S) SetSampleFrequency(freq uint32) error {
 	return nil
 }
 
+// WriteMono writes a mono audio buffer to the I2S peripheral.
 func (i2s *I2S) WriteMono(b []uint16) (int, error) {
 	return i2sWrite(i2s, b)
 }
 
+// WriteStereo writes a stereo audio buffer to the I2S peripheral.
 func (i2s *I2S) WriteStereo(b []uint32) (int, error) {
 	return i2sWrite(i2s, b)
+}
+
+// ReadMono reads a mono audio buffer from the I2S peripheral.
+func (i2s *I2S) ReadMono(p []uint16) (n int, err error) {
+	return 0, errors.ErrUnsupported
+}
+
+// ReadStereo reads a stereo audio buffer from the I2S peripheral.
+func (i2s *I2S) ReadStereo(p []uint32) (n int, err error) {
+	return 0, errors.ErrUnsupported
 }
 
 func i2sWrite[T uint16 | uint32](i2s *I2S, b []T) (int, error) {
@@ -94,10 +112,7 @@ func i2sWrite[T uint16 | uint32](i2s *I2S, b []T) (int, error) {
 	return len(b), nil
 }
 
-func (i2s *I2S) Paused(b bool) {
-	i2s.sm.SetEnabled(b)
-}
-
-func (i2s *I2S) Stop() {
-	i2s.writing = false
+// Enable enables or disables the I2S peripheral.
+func (i2s *I2S) Enable(enabled bool) {
+	i2s.sm.SetEnabled(enabled)
 }
