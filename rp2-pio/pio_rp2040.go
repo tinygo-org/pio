4215c8dc89cd944eaf6f54a46c8cce1af04136c1
Pat Whittingslow <graded.sp@gmail.com>
2026-01-25 12:56:44 -0300

    Update rp2-pio/pio_rp2040.go
    Co-authored-by: Ron Evans <ron@hybridgroup.com>

--- rp2-pio/pio_rp2040.go
+++ rp2-pio/pio_rp2040.go
@@ -65,7 +65,20 @@ func irqSetMask(mask uint32, enabled bool) {
 
 func interruptSet(nblock, irq uint8) {
 	// Need big switch since interrupt.New needs go constant for interrupt ID.
-	switch nblock {
+	switch {
+	case nblock == 0 && irq == 0:
+		interrupt.New(rp.IRQ_PIO0_IRQ_0, handleInterrupt).Enable()
+		irqSet(rp.IRQ_PIO0_IRQ_0, true)
+	case nblock == 0 && irq == 1:
+		interrupt.New(rp.IRQ_PIO0_IRQ_1, handleInterrupt).Enable()
+		irqSet(rp.IRQ_PIO0_IRQ_1, true)
+	case nblock == 1 && irq == 0:
+		interrupt.New(rp.IRQ_PIO1_IRQ_0, handleInterrupt).Enable()
+		irqSet(rp.IRQ_PIO1_IRQ_0, true)
+    case nblock == 1 && irq == 1:
+		interrupt.New(rp.IRQ_PIO1_IRQ_1, handleInterrupt).Enable()
+		irqSet(rp.IRQ_PIO1_IRQ_1, true)
+	}
 	case 0:
 		if irq == 0 {
 			interrupt.New(rp.IRQ_PIO0_IRQ_0, handleInterrupt).Enable()
